95aa925b7139abd8ccab89ce0b4dc75ac2816bf1
dimasiberia <dima.siberia@icloud.com>
2023-09-01 17:54:57 +0700

    feat: some changes

--- index.js
+++ index.js
@@ -5,12 +5,40 @@ function takeLinks() {
     links = links.replaceAll(/[\[||\]]/g, '').replaceAll(';', '\n').split('\n')
     for (let i = 0; i < links.length; i++) {
         links[i] = links[i].trim()
+        if (links[i].search(/\d[a||b||c||а||б||в]?,\s\d/) != -1) {
+            links[i] = divide(links[i])
+            console.log(links[i])
+        }
 
     }
+    let str = links.join('\n');
+    let arr = str.split('\n')
 
-    let uniqList = new Set(links)
-    let list = document.createElement('textarea')
-    let uniqListStr = Array.from(uniqList).sort().join('\n')
-    list.innerHTML = uniqListStr
-    document.body.append(list)
+    let list = document.getElementById('list')
+    let uniqList = new Set(arr)
+    let arrUniq = Array.from(uniqList).sort()
+    let uniqListStr = arrUniq.join('\n')
+    list.value = uniqListStr
+}
+function divide(str) {
+    let firstPunc = str.search(/,/);
+    let a = firstPunc;
+    let newStr = '';
+    while (firstPunc != str.length) {
+        let secPunc = str.indexOf(',', firstPunc + 1)
+        if (secPunc == -1) { secPunc = str.length }
+        let link = str.slice(0, a + 1)
+        newStr += link + str.slice(firstPunc + 1, secPunc) + '\n'
+        firstPunc = secPunc;
+    }
+}
+
+function sort() {
+    let str = document.getElementById('list').value
+    let arr = str.split('\n')
+    let list = document.getElementById('list')
+    let uniqList = new Set(arr)
+    let arrUniq = Array.from(uniqList).sort();
+    let uniqListStr = arrUniq.join('\n')
+    list.value = uniqListStr;
 }
